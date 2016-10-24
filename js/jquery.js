ca9038382c565e2542fad96ee5b649f7097ed57d
limkevin <kevinlim900@hotmail.com>
2016-10-24 04:03:16 -0400

    update6
    update6

--- js/jquery.js
+++ js/jquery.js
@@ -1,27 +1,5 @@
 $(document).ready(function($) {
-$('body').scrollToTop({
-  distance: 200,
-  speed: 1000,
-  easing: 'linear',
-  animation: 'fade', // fade, slide, none
-  animationSpeed: 500,
-
-  mobile: {
-    width: 768,
-    distance: 100,
-    speed: 1000,
-    easing: 'easeInOutElastic',
-    animation: 'slide',
-    animationSpeed: 200
-  },
-
-  trigger: null, // Set a custom triggering element. Can be an HTML string or jQuery object
-  target: null, // Set a custom target element for scrolling to. Can be element or number
-  text: 'Scroll To Top', // Text for element, can contain HTML
-
-  skin: 'cycle',
-  throttle: 250,
-
-  namespace: 'scrollToTop'
-});
+    $(".scrollTop").click(function(){
+        $('html, body').animate({ scrollTop: 0 }, 2500);
+    });
 });
